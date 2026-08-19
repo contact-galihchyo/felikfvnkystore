542c84227b201e865add781c2f52f148aa86f02a
contact-galihchyo <contact.cahyogalih@gmail.com>
2026-08-19 13:57:01 +0700

    Add files via upload

--- music.js
+++ music.js
@@ -1,16 +1,16 @@
 // Semua link preview audio online. Tambahkan/ganti link berdasarkan ID produk.
 window.MUSIC_LINKS = {
-  1: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  2: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  3: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  4: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  5: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  6: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
+  1: 'https://www.image2url.com/r2/default/audio/1787122235206-5882a211-421a-45f5-ab01-ec2668d25642.mp3',
+  2: '',
+  3: 'https://www.image2url.com/r2/default/audio/1787122034561-6bad2150-23bf-4318-8f66-5224d13106ba.mp3',
+  4: 'https://www.image2url.com/r2/default/audio/1787121979981-5af262fa-2c9c-4de2-9738-2c6fcdeb9492.mp3',
+  5: 'https://www.image2url.com/r2/default/audio/1787121738823-70d5dc9e-3eb9-4918-9159-ea4732a742dd.mp3',
+  6: 'https://www.image2url.com/r2/default/audio/1787122278752-bafe4611-b84f-4a04-aa9f-a8cca2c4d092.mp3',
   7: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  8: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  9: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  10: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  11: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  12: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3',
-  13: 'https://www.image2url.com/r2/default/audio/1787117709346-3c776fb8-505c-451d-8046-6a6e7516bd31.mp3'
+  8: 'https://www.image2url.com/r2/default/audio/1787122502534-cdd92cdf-66b0-4e30-8cf0-07647b8990a9.mp3',
+  9: 'https://www.image2url.com/r2/default/audio/1787121794373-b96c4c60-be1c-44e2-9c77-f7575afe7c2c.mp3',
+  10: '',
+  11: 'https://www.image2url.com/r2/default/audio/1787121626788-de4f37ea-9a12-4f7f-bfa4-7c603d423291.mp3',
+  12: '',
+  13: ''
 };
